4712ddf49bb2387222824ba079e0906ca636b064
Stein-Ove Bratthammer <51888431+steini1977@users.noreply.github.com>
2022-12-23 13:31:38 +0100

    Update msprite.js

--- img2c64dtjs/msprite.js
+++ img2c64dtjs/msprite.js
@@ -46,9 +46,9 @@ function calcSprite(){
 		bl =  blue(get(10+x*20,5+y*10));
 	
 		n = 0;dt=0
-		if (rd>32){n = n + pow(2,0);}
-		if (gr>32){n = n + pow(2,1);}
-		if (bl>32){n = n + pow(2,2);}			
+		if (rd>63){n = n + pow(2,0);}
+		if (gr>63){n = n + pow(2,1);}
+		if (bl>63){n = n + pow(2,2);}			
 		if (n == 1){dt=1;}
 		if (n == 2){dt=2;}
 		if (n == 4){dt=3;}
