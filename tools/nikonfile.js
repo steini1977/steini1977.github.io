319efa4b52ecf4ca8fc614577cfb184592bc42a7
Stein-Ove Bratthammer <51888431+steini1977@users.noreply.github.com>
2024-03-30 08:32:28 +0100

    Add files via upload
    creates three digit letters for todays file prename for nikon d7100 camera

---
+++ tools/nikonfile.js
@@ -0,0 +1,18 @@
+function setup(){ 
+ createCanvas(400, 400);
+  background(127);
+  let d = str(day());
+  let m = str(month());
+  print(d);
+  print(d.length);
+  if (d.length < 2) d = concat("0", d);
+  print(d);
+  print(char(65 + int(d[0])));
+  print(char(65 + int(d[1])));
+  print(char(65 + int(m)));
+  st=concat(char(65 + int(d[0])),
+            char(65 + int(d[1])));
+  st=concat(st,char(65 + int(m)));
+  textSize(160);
+  text(st, 60,240);
+}
